deee6d3a03e2e9620f268795f1bf79dc6cd3f862
manishasable1904 <sablemanisha1904@gmail.com>
2024-04-05 12:57:58 +0530

    JS simple program
    javascript simple program

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+// Camel Case: test.js, hello.js, helloWorld.js, myFirstProgram.js
+
+// Kebab: test.js, hello.js, hello-world.js, my-first-program.js
+
+// Snake: test.js, hello.js, hello_world.js, my_first_program.js
+
+var city;  //variable declaration
+city= "Pune"; //variable initialization
+
+console.log(city);
